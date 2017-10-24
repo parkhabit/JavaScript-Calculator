793079c4b6c2065e15d70d25f2c7c5c0c420befb
parkhabit <parkhabit@gmail.com>
2017-10-24 19:17:20 +0100

    added html file javascript and css

---
+++ script.js
@@ -0,0 +1,22 @@
+var box = document.getElementById('display');
+
+function toScreen(x){
+  if (x==='c'){
+    box.value='';
+  } else {
+    box.value+=x;
+  }
+}
+
+function answer(){
+  x=box.value
+  x=eval(x);
+  box.value=x;
+}
+
+function backspace(){
+  var num = box.value;
+  var len = num.length-1;
+  var newNum = num.substring(0,len);
+  box.value = newNum;
+}
